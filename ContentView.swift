dce6af354ec8b75a423a6309182bba4be95effef
MohamedArar <139015973+MohamedArar@users.noreply.github.com>
2024-04-13 17:47:30 +0300

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Memory
+//
+//  Created by Mohamed Arar on 13/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+        }
+        .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
